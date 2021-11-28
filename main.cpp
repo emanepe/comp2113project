9b71580971005d2672be11a7045c7612769a6558
Jong Seung Lee <91313537+franklee76@users.noreply.github.com>
2021-11-28 20:32:37 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,23 +12,23 @@ int main()
 {
 	do{
 		system("cls");
-    cout << "Speedy Type Game" << endl;
-    cout << "1 Start Game" << endl;
+    	cout << "Speedy Type Game" << endl;
+    	cout << "1 Start Game" << endl;
 		cout << "2 Instructions" << endl;
 		cout << "3 Saved Record" << endl;
-    cout << "4 Delete Saved" << endl;
-    cout << "5 End game" << endl;
+    	cout << "4 Delete Saved" << endl;
+    	cout << "5 End game" << endl;
 		cout << "Select Number: ";
 		char option = getche();
 
-    switch(option)
-    {
-      case '1':game();main();break;
-      case '2':help();main();break;
-      case '3':savedrecord();main();break;
-      case '4':deletesave();main();break;
-      case '5':return 0;break;
-      default:main();
-    }
+    	switch(option)
+    	{
+      		case '1':game();main();break;
+      		case '2':help();main();break;
+      		case '3':savedrecord();main();break;
+      		case '4':deletesave();main();break;
+      		case '5':return 0;break;
+      		default:main();
+    	}
 	}while(1);
 }
