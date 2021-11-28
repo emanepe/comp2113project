996d246eb7cce91c78f364b11b492e59b8101102
Jong Seung Lee <91313537+franklee76@users.noreply.github.com>
2021-11-28 20:21:31 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
-#include<time.h>
-#include<conio.h>
-#include<stdio.h>
-#include<windows.h>
-#include<dos.h>
-#include<stdlib.h>
+#include <iostream>
+#include <conio.h>
+#include <dos.h>
+#include <windows.h>
+#include <time.h>
+#include <string.h>
+#include "gamefunc.h"
 
+using namespace std;
+
+int main()
+{
+	do{
+		system("cls");
+    cout << "Speedy Type Game" << endl;
+    cout << "1 Start Game" << endl;
+		cout << "2 Instructions" << endl;
+		cout << "3 Saved Record" << endl;
+    cout << "4 Delete Saved" << endl;
+    cout << "5 End game" << endl;
+		cout << "Select Number: ";
+		char option = getche();
+
+    switch(option)
+    {
+      case '1':game();main();break;
+      case '2':help();main();break;
+      case '3':savedrecord();main();break;
+      case '4':deletesave();main();break;
+      case '5':return 0;break;
+      default:main();
+    }
+	}while(1);
+}
