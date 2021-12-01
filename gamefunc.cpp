2125d9c6d25ce101f3cf201245efdb8038952aa8
Jong Seung Lee <91313537+franklee76@users.noreply.github.com>
2021-12-02 02:33:45 +0800

    Update gamefunc.cpp

--- gamefunc.cpp
+++ gamefunc.cpp
@@ -1,57 +1,48 @@
 #ifndef GAMEFUNC_H
 #define GAMEFUNC_H
 
-//function to calculate current user score
-void score(){
-	struct User{
-		string name;
-		int scores[5];
-		int numScores;
-}
-}
-
 //function to measure current user accuracy
-void accuracy(){
+void accuracy() {
 	int c_word;
 	int inc_word = 0;
 	if (c_word != g_word)
 		++inc_word;
-	
-	accuracy=g_word-inc_word; //incorrect word 
-	accuracy=accuracy/g_wordTotal; //전체 게임출제단어 array 크기구해서 나누기
-	accuracy=accuracy*100;
-	
+
+	accuracy = g_word - inc_word; //incorrect word 
+	accuracy = accuracy / g_wordTotal; //전체 게임출제단어 array 크기구해서 나누기
+	accuracy = accuracy * 100;
+
 }
 
 //function to measure user typing speed
-void typingspeed(){
-  using Clock = chrono::high_resolution_clock;
-  cout << "Type: ";
-  cin >> string answer;
-  auto start = Clock::now();
-  cin >> answer;
-  auto end = Clock::now();
-  auto s = chrono::duration_cast<chrono::seconds>(end - start).count();
+void typingspeed() {
+	using Clock = chrono::high_resolution_clock;
+	cout << "Type: ";
+	cin >> string answer;
+	auto start = Clock::now();
+	cin >> answer;
+	auto end = Clock::now();
+	auto s = chrono::duration_cast<chrono::seconds>(end - start).count();
 
-  cout << "You took " << s << " seconds" << endl;  //print user input time for the previous chance
+	cout << "You took " << s << " seconds" << endl;  //print user input time for the previous chance
 }
 }
 
 //help() is to show the instructions of the game
-void help(){
-  system("cls");
-  cout << "This is a game designed to improve your typing speed." << endl;
-  cout << "User can select appropriate difficulties with different time limits." << endl;
-  cout << "User can also choose numbers of word "<< endl;
-  cout << "User has to type a given word within time limit, maintaining certain level of accuracy." << endl;
-  cout << "User can also view their highest record, and this can be reset if the user want." << endl;
-  cout << "Please enjoy and type any key to exit: ";
-  getche();
+void help() {
+	system("cls");
+	cout << "This is a game designed to improve your typing speed." << endl;
+	cout << "User can select appropriate difficulties with different time limits." << endl;
+	cout << "User can also choose numbers of word to challenge. (20, 30, 40 words)" << endl;
+	cout << "User has to type a given word within time limit, maintaining certain level of accuracy." << endl;
+	cout << "User can also view their highest record, and this can be reset if the user want." << endl;
+	cout << "Please enjoy and type any key to exit: ";
+	getche();
 }
 
 //saverecord() can display user's highest score, acculmulative average accuracy, average typing speed.
-void savedrecord(){
-  FILE *fp2 = NULL;
+void savedrecord() {
+	FILE *fp2 = NULL;
 	double s;
 	int cnt;
 	system("cls");
@@ -72,62 +63,91 @@ void savedrecord(){
 	getch();
 }
 
-//sets time limit for playing game
-void set_time() {
-	int ch;
-	system("cls");
-	pos;
-	printf("Set the difficulty(time):");
-	pos;
-	ln
-	pos;
-	pos;
-	printf("press 1 for Easy") ;pos;
-	printf("press 2 for Intermediate"); pos;
-	printf("press 3 for Hard");pos;
-	printf("press 4 for Extremely Hard");pos;
-	scanf("%d",&ch);
-	switch(ch){
-		case 1:t=30;break;
-		case 2:t=20;break;
-		case 3:t=10;break;
-		case 4:t=5;break;
-		default:set_time();
-	}
+void deletesave() {
+	//tranche
 }
 
-void deletesave(){
+void game() {
+	int timelimit;
+	int word = 30;
 
-}
-
-void game(){
-  int level, diff, accuracy=0, score=0, count=0;
-  clock_t begin;
-  int time_spent, speed;
-  system("cls");
-  cout << "Select Difficulty of the game:" << endl;
-  cout << "1 Easy" << endl;
-  cout << "2 Intermediate" << endl;
-  cout << "3 Hard" << endl;
-  cout << "4 Extremely Hard" << endl;
-  cout << "Select Number: "
-  char difficulty = getche();
-  switch(difficulty)
-  {
-    case '1':diff=1;break;
-    case '2':diff=2;break;
-    case '3':diff=3;break;
-    case '4':diff=4;break;
-    default:game();
-  }
-  system("cls");
-  cout << "Select numbers of words you want to challenge:" << endl;
-  while(){
-		//Actual game
-	}
-	
+	//Difficulty setting (adjusting time limit for each word)
 	system("cls");
-	cout<<"Game Over"<<endl;
+	cout << "Select difficulty of the game:" << endl;
+	cout << "1 Easy" << endl;
+	cout << "2 Intermediate" << endl;
+	cout << "3 Hard" << endl;
+	cout << "4 Extremely Hard" << endl;
+	cout << "Select Number: "
+	char difficulty = getche();
+	switch (difficulty)
+	{
+	case '1':timelimit = 1; break;
+	case '2':timelimit = 2; break;
+	case '3':timelimit = 3; break;
+	case '4':timelimit = 4; break;
+	default:game();
+	}
+
+	//Word number setting (default: 30 words, can be varied to 20 or 40)
+	system("cls");
+	cout << "Select numbers of words you want to challenge:" << endl;
+	cout << "1 20 words" << endl;
+	cout << "2 30 words" << endl;
+	cout << "3 40 words" << endl;
+	cout << "Select Number: "
+	char userword = getche();
+	switch (userword)
+	{
+	case '1':word = 20; break;
+	case '2':word = 40; break;
+	default:break;
+	}
+
+	//File input of the word lists - 520 words(20 words for each alphabet) - use of dynamic array
+	ifstream fin("input.txt");
+	vector<string> wordlist;
+	string word;
+	while (getline(fin, word))
+		wordlist.push_back(word);
+
+	//Initialize user progress variables
+	int totalword = 0;
+	int correctword = 0;
+	float totaltime = 0;
+	float starttime, endtime;
+
+	//boolean variable to determine whether it is game over or stage success from the user
+	bool success = false;
+
+	//Actual game function
+	do{
+		system(:"cls");
+		starttime = clock(); //record starting time
+		int randindex = rand() % wordlist.size(); //randomly select index to choose a word to test
+		string testingt = wordlist(randindex);
+
+		cout << testingt << endl;
+		string userinput;
+		do {
+			float point = clock() / CLOCKS_PER_SEC;
+			if ((point - starttime) >= timelimit)
+				break;
+		} while (input 받을때까지); //apply time limit while user input (다시 돌아와서 구현)
+		if () {
+			
+		}
+		else {
+			
+		}
+
+
+		endtime = clock() - starttime;
+		delay(1sec);
+	} while (); //termination condition ()
+
+	system("cls");
+	cout << "Game Over" << endl;
 	fp = fopen("score.txt", "a"); // 점수 저장 파일 열기
 	if (fp == NULL) // 파일 열기 오류
 		printf("점수 기록 파일 작성 실패!\n\n");
