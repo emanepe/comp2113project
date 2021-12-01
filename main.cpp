ff21d9bc756b815d29b4c5f2b175e004c13f5c6d
mjjo <91597547+emanepe@users.noreply.github.com>
2021-12-02 03:24:48 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -59,30 +59,5 @@ int main(){
       		default:main();
     	}
 	}while(1);
-		x = 조건; 유저인풋이랑 단어같아야됨;    // 
-		old_time = clock();    // 시작 시간 
-        	use_inp = 1;         // 키입력 초기값 
- 
-        	printf(x,y);
- 
-        do{ 
-            new_time = clock();    // 현재 시간 
-            if (difftime(later_time, first_time) > DELAY) {    // 시간 초과 검사, 딜레이로 시간 바꿔야됨
-                ans = 0;        // 답이 없음 
-                use_inp = 0;        // 키입력이 없음 
-                break;
-            }
-        } while (!kbhit());        // 키가 안 눌린 동안 
- 
- 
-        if (use_inp == 1) scanf("%d", &answer);    // 답을 받음 
-    
-        if(sum == answer){
-            printf("Correct");
-        }
-        else{
-            printf("Incorrect, \n total correct is \n", sum, yes);
-            return 0;
-        } 
-        yes++;  // 맞힌갯수
+
 }
