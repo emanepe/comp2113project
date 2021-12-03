c06303a149088f584f6641911b6ac064a7ebb211
Jong Seung Lee <91313537+franklee76@users.noreply.github.com>
2021-12-03 16:05:49 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,13 @@
 #include <iostream>
-#include <fstream>
-#include <cstdio>
-#include <ctime>
-#include <cstring>
-#include <cstdlib>
-#include <vector>
 #include "gamefunc.h"
 
 using namespace std;
 
 int main() {
-
+	bool run = true;
 	do {
-		system("cls");
-		cout << "Typing Game" << endl;
+		system("clear");
+		cout << "############# Typing Game #############" << endl;
 		cout << "---------------------------------------" << endl;
 		cout << "produced by Jong Seung Lee & Min Jae Jo" << endl;
 		cout << "---------------------------------------" << endl;
@@ -23,17 +17,19 @@ int main() {
 		cout << "3 Saved Record" << endl;
 		cout << "4 Delete Saved" << endl;
 		cout << "5 End game" << endl;
-		cout << "Select Number: ";
-		char option = getche();
+		cout << "Select Number:" << endl; //user selecting options, input number 1, 2, 3, 4, 5
+		char option = getchar();
 
 		switch (option)
 		{
-		case '1':game(); main(); break;
-		case '2':help(); main(); break;
-		case '3':savedrecord(); main(); break;
-		case '4':deletesave(); main(); break;
-		case '5':return 0; break;
-		default:main();
+			case '1':game(); break; //load game function
+			case '2':help(); break; //load instruction functon
+			case '3':savedrecord(); break; //load saved record function
+			case '4':deletesave(); break; //load deleting save record function
+			case '5':run=false; break; //terminate while loop
+			default: break; //ignore the rest
 		}
-	} while (1);
+	} while (run); //termination condition: until user select option 5 End game
+
+	return 0;
 }
